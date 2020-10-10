117896ab40e3060b72271d065f1278245f7067fa
fulder <misad90@gmail.com>
2020-10-10 19:04:14 +0200

    Enable plex auto update before running update_pma in post_install

--- post_install.sh
+++ post_install.sh
@@ -2,15 +2,15 @@
 
 mkdir -p /usr/local/etc/rc.d
 
+echo "Enable plex auto update"
+/usr/local/bin/pmsset auto_update true
+/usr/local/bin/pmsset cron "0 5 * * *"
+
 echo "Starting update_pma script from post_install"
 /usr/local/bin/update_pma
 
 echo "Start pms service"
 sysrc -f /etc/rc.conf pms_enable="YES"
 service pms start
-
-echo "Enable plex auto update"
-/usr/local/bin/pmsset auto_update true
-/usr/local/bin/pmsset cron "0 5 * * *"
 
 echo "Post install completed!"
