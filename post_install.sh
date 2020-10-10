0fd6954833b99deb54711f272c9cca2cf154cce1
fulder <misad90@gmail.com>
2020-10-10 19:33:56 +0200

    Set pms_enable to YES before running update script in post_install

--- post_install.sh
+++ post_install.sh
@@ -2,15 +2,12 @@
 
 mkdir -p /usr/local/etc/rc.d
 
-echo "Enable plex auto update"
+echo "Enable service"
+sysrc -f /etc/rc.conf pms_enable="YES"
 /usr/local/bin/pmsset auto_update true
 /usr/local/bin/pmsset cron "0 5 * * *"
 
 echo "Starting update_pms script from post_install"
 /usr/local/bin/update_pms
-
-echo "Start pms service"
-sysrc -f /etc/rc.conf pms_enable="YES"
-service pms start
 
 echo "Post install completed!"
