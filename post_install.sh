4cc7c995a8927487b5e8a0408cff3d60b313c463
fulder <misad90@gmail.com>
2020-10-10 13:25:53 +0200

    Add post_install script using download_latest and starting up service

---
+++ post_install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p /usr/local/etc/rc.d
+
+chmod +x /usr/local/etc/rc.d/pms
+chmod +x /usr/local/pms/download_latest.sh
+
+echo "Starting download_latest script from post_install"
+source /usr/local/pms/download_latest.sh
+
+echo "Start pms service"
+chmod +x /usr/local/etc/rc.d/pms
+sysrc -f /etc/rc.conf pms_enable="YES"
+service pms start
+
+echo "Post install completed!"
