2403482fc2122a7ae278725af4f3f758be4a1006
fulder <misad90@gmail.com>
2020-10-10 13:25:34 +0200

    Add script for downloading and updating latest plex freebsd version

---
+++ overlay/usr/local/bin/download_latest.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Finding latest plex version"
+fetch https://plex.tv/pms/downloads/5.json
+download_url=$(cat 5.json | jq -r '.computer.FreeBSD.releases[0].url')
+version=$(cat 5.json | jq -r '.computer.FreeBSD.version')
+rm 5.json
+
+echo "Downloading plex server with version: ${version}"
+fetch "${download_url}"
+
+echo "Updating pms"
+rm -rf /usr/local/pms
+tar xzf "PlexMediaServer-${version}-FreeBSD-amd64.tar.bz2" /usr/local/pms
+rm "PlexMediaServer-${version}-FreeBSD-amd64.tar.bz2"
+
+echo "Download latest script complete"
